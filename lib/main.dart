7dab0e4c03c03bb460feecc3ee8a718298901a4a
Aminullah <aminullahbscs@gmail.com>
2025-04-18 04:54:23 -0700

    assignment No:3 of arithmetic operator

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,35 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
+
 
 void main(){
-  // String userChoice;
-  // print('Enter password strength');
-  // userChoice= stdin.readLineSync()!;
-  // print(userChoice);
-  // chosePassword(userChoice);
 
-  Map<String, dynamic> birthday={
-    'amin': "2/2/2333",
-    'aman': "2/2/2333",
-    'aslam': "2/2/2333",
-    'tariq': "2/2/2333",
-  };
+var a, b, result;
+  a=7;
+  b=3;
+  print('First Number a: $a');
+  print("Second Number b: $b");
 
-  birthday.forEach((k,v)=>print("$k,$v"));
+
+  result = a+b;
+  print("Sum of $a and $b and is $result");
+
+result = a-b;
+print("Difference of $a and $b and is $result");
+
+result = (a/b);
+print("division of $a and $b and is $result");
+
+result = (a~/b);
+print("integer division of $a and $b and is $result");
+
+result = (a%b);
+print("Reminder of $a and $b and is $result");
+
+result = (a*b);
+print("Multiplication of $a and $b and is $result");
+
 }
-generatePassword(int number){
-  var random = Random.secure();
-  List<int> r=List.generate(number,(_) => random.nextInt(20));
-  List resultList=base64UrlEncode(r).split('').toList();
-  print(resultList.join());
-}
-chosePassword(String userChoice){
-  if(userChoice== 'we'){
-    generatePassword(5);
-  }
-  else if(userChoice== 'md'){
-    generatePassword(10);
-  } else if(userChoice== 'st'){
-    generatePassword(15);
-  }else{
-    print('incorrect choice');
-  }
-}
+
+
+
+
+
