cbb847467bb0ad8e223b3b15b16de293055c78f4
Aminullah <aminullahbscs@gmail.com>
2025-04-29 05:25:03 -0700

    assignment No:4 if and else if

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,34 @@
 import 'dart:io';
 
 void main(){
-  int a, b, n, x;
+int x, y;
+  print('enter x');
+x= int.parse(stdin.readLineSync()!);
+print('enter y');
+y= int.parse(stdin.readLineSync()!);
 
-    // a number is odd or even
-      print('Enter a Number');
-      n= int.parse(stdin.readLineSync()!);
-      if(n%2==0){
-        print('the Number is even');
-      }else{
-        print('the Number is odd');
-      }
+if(x==0 && y==0){
+  print("origin");
+}else if(x>0 && y==0){
+  print(" x axis");
+}
+else if(x==0 && y>0){
+  print(" y axis");
+}
+else if(x>0 && y>0){
+  print(" first quadrant");
+}
+else if(x<0 && y>0){
+  print("Second quadrant");
+}else if(x<0 && y<0){
+  print("third quadrant ");
+}
+else if(x>0 && y<0){
+  print("fourth quadrant ");
+}
 
-      // a number is positive or negative
-  print('Enter a Number');
-  x= int.parse(stdin.readLineSync()!);
-  if(x>0){
-    print('a Number positive');
-  }
-else if(x<0){
-    print('a Number negative');
-  }
-else{
-    print('a Number is zero');
-  }
+}
 
-  // the smallest in 2 number
-     print('Enter first Number');
-     a= int.parse(stdin.readLineSync()!);
-     print('Enter Second Number');
-     b= int.parse(stdin.readLineSync()!);
-     if(a<b){
-       print(" $a is small number ");
-     }else{
-       print('$b is small number');
-     }
-   }
 
 
 
